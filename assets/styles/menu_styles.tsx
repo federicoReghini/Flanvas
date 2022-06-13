064495a4eca5c3c8e6e5604fa799e18ca3c8e3ea
ggDev95 <devotiluca95@gmail.com>
2022-06-13 16:29:55 +0200

    Created menu styles and added container flexRow

---
+++ assets/styles/menu_styles.tsx
@@ -0,0 +1,9 @@
+import { PixelRatio, StyleSheet } from "react-native";
+
+export const menu_styles = StyleSheet.create({
+
+    paletteWrapper: {
+        width: 40, 
+        height: 40
+    },
+})
