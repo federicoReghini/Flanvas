a95e969e874b22b2b6261d8a2433ef50e5b80e7a
ggDev95 <devotiluca95@gmail.com>
2022-06-13 16:30:53 +0200

    Added menu logic

--- components/hookComponents/Menu.tsx
+++ components/hookComponents/Menu.tsx
@@ -1,49 +1,103 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState, useEffect, ReactElement } from 'react';
 
 // native components
 import { View, Text, Pressable } from 'react-native';
 
 // style
 import { styles } from '../../styles_generic';
+import { menu_styles } from '../../assets/styles/menu_styles';
 
 // type
 import { signatureRef } from '../../utils/ts/types';
 
-interface Props  {
-    refCanvas: signatureRef
+
+interface Props {
+    refCanvas: signatureRef,
+    callback: () => void
 }
 
-const Menu: FunctionComponent<Props> = ({ refCanvas }) => {
+interface State {
+    isPalette: boolean
+}
 
-    const handleCallback = (ref: signatureRef, params: any) => () => {
+const initState = {
+    isPalette: false
+}
+
+
+
+const palette = ['#000000', '#FF4848', '#387CFF', '#3CFF72', '#D560FE', '#FF8A00']
+
+
+
+const Menu: FunctionComponent<Props> = ({ refCanvas, callback }) => {
+
+    const [state, setState] = useState<State>(initState)
+
+    let isModal: boolean = false;
+
+    /*     useEffect( //callback, [isModal]) */
+
+
+    const handleCallback = (ref: any, params: any) => () => {
+
+        isModal = true
+
         switch (ref) {
 
-            case refCanvas?.changePenColor: return refCanvas?.changePenColor(params);
+            case refCanvas?.changePenColor: return refCanvas?.changePenColor(params)
             case refCanvas?.changePenSize: return refCanvas?.changePenSize(params, params);
             case refCanvas?.clearSignature: return refCanvas?.clearSignature();
 
             default:
                 return null;
         }
+
+    }
+
+    const handlePalette = (): void => {
+        setState({
+            ...state,
+            isPalette: !state.isPalette
+        })
+    }
+
+    const map = (color: string, key: number): ReactElement => {
+        return (
+            <Pressable
+                key={key}
+                style={menu_styles.paletteWrapper}
+                onPress={handleCallback(refCanvas?.changePenColor, color)}>
+                <View style={{ backgroundColor: color, width: 20, height: 20 }}></View>
+            </Pressable>
+        )
     }
 
     return (
 
         <View>
-            <Pressable
-                onPress={handleCallback(refCanvas.changePenColor, 'red')}
+            <Pressable onPress={handlePalette}
             >
+
                 <Text style={styles.btn}>Color</Text>
             </Pressable>
+            <View style={styles.containerFlexRow}>
+
+                {
+                    state.isPalette &&
+                    palette.map(map)
+                }
+                
+            </View>
 
             <Pressable
-                onPress={handleCallback(refCanvas.changePenSize, 3)}
+                onPress={handleCallback(refCanvas?.changePenSize, 3)}
             >
                 <Text style={styles.btn}>Pen Size</Text>
             </Pressable>
 
             <Pressable
-            onPress={handleCallback(refCanvas.clearSignature, null)}
+                onPress={handleCallback(refCanvas?.clearSignature, null)}
             >
                 <Text
                     style={styles.btn}
